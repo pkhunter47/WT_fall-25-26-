62db88bb42915f313b426defaffdfdc8ff636e6e
Protik Biswas <154260246+pkhunter47@users.noreply.github.com>
2025-11-01 21:01:02 +0600

    Add files via upload

---
+++ Lab1.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>My first program practice</title>
+</head>
+<body>
+    <center>
+<h1>My first program in web tech</h1>
+
+
+    </center>
+    <p> welcome to the heaven!!! <br> <br> this course is not for you if you have a plan to do chill during full course</p>
+    <br> <br> <br>
+    Enter your name:
+    <input type ="text">
+    <br><br>
+     Enter your age:
+    <input type ="number">
+    <br> <br>
+     Enter your DOB:
+    <input type ="date">
+    <br> <br> <br>
+    language:
+     <input type ="radio">bangla
+      <input type ="radio">english
+      <br> 
+      semester:
+       <input type ="checkbox">1st
+        <input type ="checkbox">2nd
+     <center>
+       <input type ="submit">
+       <br> <br>
+        <input type ="reset">
+         <br><br>
+          </center>
+          <br>
+          
+
+    <input type="button">
+        
+
+    
+</body>
+</html>
