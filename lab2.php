8b8095da15f7c0a046388a3e9705564f23369629
pkhunter47 <protikbiswas3099@gmail.com>
2025-11-01 22:04:49 +0600

    lab2added

---
+++ lab2.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<head>
+    <title>My web tech</title>
+</head>
+
+
+<body>
+    <style>
+       h1{
+        color: blue;
+        font-size: 50px;
+       }
+       p{
+        font-size: 30px;
+        color: green;
+        text-align: center;
+        font-family: Italic;
+
+       }
+       td{
+        font-size: 20px;
+        color: blue;
+        font-family: "Times New Roman";
+    }
+    h3{
+        color: black;
+        font-size: 15px;
+    }
+    h4{
+        color: black;
+        font-size: 15px;
+    }
+    </style>
+<center>
+<h1>This is my website</h1>
+</center>
+<br><br><br>
+<center>
+    <table>
+    <tr>
+        <td>
+            <center>
+                <p>Login Form helo</p>
+            </center>
+            
+        </td>
+    </tr>
+    <tr>
+        <td>
+           Username:
+            <input type="text" placeholder="Enter your username">
+        </td>
+    </tr>
+        <tr>
+        <td>
+           Password:
+            <input type="password" placeholder="Enter your password">
+
+        </td>
+</tr>
+<tr>
+    <td>
+        <h3><input type="checkbox">Remember me</h3>
+    </td>
+    <td>
+        <h4><input type="radio" >Forgot Password</h4>
+    </td>
+</tr>
+<tr>
+    <td>
+        <br>
+        <input type="submit" value="Login">
+        <br>
+        <td><input type="reset" value="Reset now confirm"></td>
+    </td>
+</tr>
+</table>
+
+
+</center>
+</body>
+</html>
