ac1573a4ec9640ede56c7d8ae68e08e35f372aab
pkhunter47 <protikbiswas3099@gmail.com>
2025-11-17 10:59:41 +0600

    started

--- MID/lab task/lab task2.php
+++ MID/lab task/lab task2.php
@@ -2,8 +2,36 @@
 
 <head>
     <title>Participant Registration</title>
+    <style>
+    body{
+    background-color: rgb(161, 209, 249);;
+}
+#form1{
+   
+
+    background-color: aliceblue;
+    margin: 0 auto;
+    box-shadow:  0 0 10px rgba(0,0,0,0.1);
+    border-radius: 10px;
+    padding: 20px;
+    width: 300px;
+    height: 390px;
+  
+}
+
+
+    </style>
 </head>
 <body>
+    <form id="form1">
+        <h2>Participant Register</h2>
+        <label>Full name:</label>
+
+        
+
+
+
+    </form>
     
 </body>
 </html>
