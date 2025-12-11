03730d25fc374602e2e5052fc04f82052e4819f5
pkhunter47 <protikbiswas3099@gmail.com>
2025-12-11 20:13:14 +0600

    practice of validation

---
+++ Final/class/practice2php.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+<head><title>PHP Code</title></head>
+ 
+<body>
+<h1>This is our 1st php code </h1>    
+ 
+ 
+<?php
+ 
+$name= "";
+$nameerror= "";
+ 
+if (empty ($_POST["name"]))
+{
+$nameerror="Name is req"; // value empty
+}
+else{
+$name= test_input($_POST["name"]); //clean out input
+if (!preg_match("/^[a-zA-Z ]*$/",$name))  // only letter and middle space
+{
+    $nameerror ="Only letters and white space allowed";
+}
+ 
+}
+ 
+function test_input($data)
+{
+$data = trim($data); // trim amr previous data remove kore dai
+return $data;
+}
+ 
+?>
+ 
+ 
+<form method="post" action="">
+ 
+Name: <input type="text" name="name" value="<?php echo $name;?>">
+<?php echo $nameerror; ?>
+ 
+Age: <input type="number" name="age" >
+ 
+Email : <input type="text" name="email">
+ 
+ 
+<input type="submit" name="submit" value="Submit">
+</form>
+ 
+ 
+<?php
+if($_SERVER["REQUEST_METHOD"]== "POST" && empty($nameerror))
+{
+echo "<h3> Your Input: </h3>";
+echo "Name: ".$name. "<br>";
+ 
+}
+ 
+?>
+ 
+ 
+ 
+ 
+</body>
+</html>
+ 
---
+++ Final/class/practicephp.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Php code</title>
+</head>
+<body>
+    <h1>This is our 1st php</h1>
+    
+</body>
+<?php
+echo "Hello World <br>";
+
+$name="protik";
+$age="25";
+
+echo "I am " .$name. " and My age is: ".$age;
+echo "<br>";
+
+$a=30;
+$b=60;
+
+if($a>$b){
+
+    echo "a is bigger <br>";
+
+}
+else{
+    echo "b is bigger <br>";
+}
+echo "HELLO me <br>";
+echo "New Line <br>";
+
+?>
+
+
+</html>
