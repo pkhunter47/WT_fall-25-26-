84315aa7f2bb8ac5e566c8973254a41935ee1af3
pkhunter47 <protikbiswas3099@gmail.com>
2025-12-11 19:13:11 +0600

    some changes

---
+++ Final/Project/mainwebpage.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<head>
+
+    <title>HappyLife</title>
+</head>
+<link rel="stylesheet" href="mainwebpagedesign.css">
+<body>
+    <div>
+        Welcome to HappyLife
+    </div>
+    
+</body>
+</html>
