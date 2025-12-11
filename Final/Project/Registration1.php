b72190836ab4fdda7d1fefa5dbde0decd98d9686
pkhunter47 <protikbiswas3099@gmail.com>
2025-12-12 01:47:26 +0600

    doing reg form

---
+++ Final/Project/Registration1.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<hrml>
+<head>
+    <title>Registration Form</title>
+</head>
+<link rel="stylesheet" href="designreg.css">
+<body>
+    <form id="form1">
+        <img src="11879738.png" alt="registration" srcset="" width="80px" height= "50px" id="regimage">
+        <center>
+
+    <h2>Register NOW!</h2>
+    </center>
+  
+
+     <label style="font-family: Times New Roman,Times, serif;" >Student-ID:</label>
+    <input type="number"id="id">
+    <label style="font-family: Times New Roman,Times, serif;" >Name:</label>
+    <input type="text"id="name">
+
+    <label style="font-family: Times New Roman,Times, serif;" >E-mail:</label>
+    <input type="text"id="email">
+
+    <label style="font-family: Times New Roman,Times, serif;" >Phone-Number:</label>
+    <input type="number"id="phonenumber">
+
+    <label style="font-family: Times New Roman,Times, serif;" >Password:</label>
+    <input type="password"id="password">
+
+    <label style="font-family: Times New Roman,Times, serif;" >Confirm password:</label>
+    <input type="password"id="c_password">
+
+
+
+    </form>
+    
+    
+</body>
+</html>
