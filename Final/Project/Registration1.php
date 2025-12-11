6e47d008eb37a9f0e5798cb5de9cd1a15de1dac8
pkhunter47 <protikbiswas3099@gmail.com>
2025-12-12 03:29:32 +0600

    some validation are added for today

--- Final/Project/Registration1.php
+++ Final/Project/Registration1.php
@@ -5,8 +5,8 @@
 </head>
 <link rel="stylesheet" href="designreg.css">
 <body>
-    <form id="form1">
-        <img src="11879738.png" alt="registration" srcset="" width="80px" height= "50px" id="regimage">
+    <form id="form1" method="post">
+        <img src="11879738.png" alt="registration" srcset="" width="60px" height= "50px" id="regimage">
         <center>
 
     <h2>Register NOW!</h2>
@@ -14,25 +14,37 @@
   
 
      <label style="font-family: Times New Roman,Times, serif;" >Student-ID:</label>
-    <input type="number"id="id">
+    <input type="number"id="id" name="id" required>
     <label style="font-family: Times New Roman,Times, serif;" >Name:</label>
-    <input type="text"id="name">
+    <input type="text"id="name" name="name" required>
 
     <label style="font-family: Times New Roman,Times, serif;" >E-mail:</label>
-    <input type="text"id="email">
+    <input type="text"id="email" name="email" required>
 
     <label style="font-family: Times New Roman,Times, serif;" >Phone-Number:</label>
-    <input type="number"id="phonenumber">
+    <input type="number"id="phonenumber" name="phonenumber" required>
 
     <label style="font-family: Times New Roman,Times, serif;" >Password:</label>
-    <input type="password"id="password">
+    <input type="password"id="password" name="password" required>
 
     <label style="font-family: Times New Roman,Times, serif;" >Confirm password:</label>
-    <input type="password"id="c_password">
+    <input type="password"id="c_password" name="c_password" required>
+
+    <button type="submit" id="button">Register</button>
+
 
 
 
     </form>
+
+    <div id="error" name="error">
+    </div>
+
+    <?php
+
+    include 'validationofreg.php';
+
+    ?>
     
     
 </body>
---
+++ Final/Project/validationofreg.php
@@ -0,0 +1,43 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $id= $_POST["id"];
+    $name= $_POST['name'];
+    $email= $_POST['email'];
+    $phonenumber= $_POST['phonenumber'];
+    $password= $_POST['password'];
+    $c_password= $_POST['c_password'];
+    $error="";
+
+
+   
+    if ($password !== $c_password) {
+
+        $error="Passwords are not  correct!";
+        echo $error;
+        exit;
+        
+    }
+   
+    if(!preg_match("/^[a-zA-Z ]*$/",$name)){
+        echo "Names are allowed only";
+        exit;
+
+    }
+
+    else{
+
+
+    echo "<h2>Registration Successful</h2>";
+
+
+    echo "Student ID: " . $id . "<br>";
+    echo "Name: " .$name. "<br>";
+    echo "Email: " .$email. "<br>";
+    echo "Phone Number: " .$phonenumber. "<br>";
+    echo "Password: " .$password. "<br>";
+
+    exit;
+
+    }
+}
+?>
