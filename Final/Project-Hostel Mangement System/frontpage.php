9491710d54be5ca842afcb2002e9c9fb4d4a489e
pkhunter47 <protikbiswas3099@gmail.com>
2025-12-16 23:28:51 +0600

    some changes

---
+++ Final/Project-Hostel Mangement System/frontpage.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+
+<head>
+    <title>Happy Life</title>
+    <link rel="stylesheet" href="frontpagedesign.css">
+</head>
+<body>
+    <div id="header">
+        <h1>Welcome to Happy-Life Hostel</h1>
+
+    </div>
+    
+</body>
+</html>
