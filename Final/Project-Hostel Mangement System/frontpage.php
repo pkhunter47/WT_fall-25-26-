f26e421eb8f2cffac07e439bf1954ad4abdf04ce
pkhunter47 <protikbiswas3099@gmail.com>
2025-12-17 00:06:48 +0600

    header done

--- Final/Project-Hostel Mangement System/frontpage.php
+++ Final/Project-Hostel Mangement System/frontpage.php
@@ -6,7 +6,11 @@
 </head>
 <body>
     <div id="header">
-        <h1>Welcome to Happy-Life Hostel</h1>
+        <img src="building-vector-icon-white-background_855620-52.avif" alt="Logo" id="logo">
+        <h1><b><i>Welcome to Happy-Life</i></b></h1>
+
+        <button type="submit" id="register">Register Now</button>
+        <button type="submit" id="login">Login</button>
 
     </div>
     
