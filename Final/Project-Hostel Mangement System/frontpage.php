3b1dd27369f72baa3927f35ff8dfb2d9d187f71a
pkhunter47 <protikbiswas3099@gmail.com>
2025-12-17 02:19:10 +0600

    some codes added

--- Final/Project-Hostel Mangement System/frontpage.php
+++ Final/Project-Hostel Mangement System/frontpage.php
@@ -9,10 +9,27 @@
         <img src="building-vector-icon-white-background_855620-52.avif" alt="Logo" id="logo">
         <h1><b><i>Welcome to Happy-Life</i></b></h1>
 
-        <button type="submit" id="register">Register Now</button>
+        <button type="button" id="register" onclick="handle()">Register Now</button>
         <button type="submit" id="login">Login</button>
 
     </div>
+    <br>
+
+    <div id="list">
+
+    <ul>
+    ⭐Our Facilities:
+        <li>Free-wifi</li>
+        <li>Generator</li>
+        <li>Cleanliness</li>
+        <li>Laundry service</li>
+        <li>24/7 security</li>
+        <li>Study room</li>
+        <li>3 times meals</li>
+        <li>Medical facility</li>
+    </ul>
+    </div>
+    <script src="frontpagejs.js"></script>
     
 </body>
 </html>
---
+++ Final/Project-Hostel Mangement System/frontpagejs.js
@@ -0,0 +1,6 @@
+function handle(){
+    if(confirm("Are you sure you want to register?")){
+        window.location.href = "USER/HTML/registration1.php";
+        
+    }
+}
