c5a640361bd6ab1a6b6f394f576f0c0a31ae6aa5
pkhunter47 <protikbiswas3099@gmail.com>
2025-12-21 01:23:53 +0600

    few changes and validatioin using php

--- Final/Project-Hostel Mangement System/USER/HTML/Registration1.php
+++ Final/Project-Hostel Mangement System/USER/HTML/Registration1.php
@@ -1,5 +1,10 @@
+ <?php
+
+    include '../PHP/validationonreg.php';
+
+    ?>
 <!DOCTYPE html>
-<hrml>
+<html>
 <head>
     <title>Registration Form</title>
 </head>
@@ -14,56 +19,74 @@
   
 
      <label style="font-family: Times New Roman,Times, serif;" >Student-ID:</label>
-    <input type="number"id="id" name="id" required>
+    <input type="number"id="id" name="id" value="<?php echo $id; ?>">
+ 
+
     <label style="font-family: Times New Roman,Times, serif;" >Name:</label>
-    <input type="text"id="name" name="name" required>
+    <input type="text"id="name" name="name" value="<?php echo $name; ?>">
+    
 
     <label style="font-family: Times New Roman,Times, serif;" >E-mail:</label>
-    <input type="text"id="email" name="email" required>
+    <input type="text"id="email" name="email" value="<?php echo $email; ?>">
+    
 
     <label style="font-family: Times New Roman,Times, serif;" >Phone-Number:</label>
-    <input type="number"id="phonenumber" name="phonenumber" required>
+    <input type="number"id="phonenumber" name="phonenumber" value="<?php echo $phonenumber; ?>">
+    
 
     <label style="font-family: Times New Roman,Times, serif;" >Password:</label>
-    <input type="password"id="password" name="password" required>
+    <input type="password"id="password" name="password" value="<?php echo $password; ?>">
+    
 
     <label style="font-family: Times New Roman,Times, serif;" >Confirm password:</label>
-    <input type="password"id="c_password" name="c_password" required>
+    <input type="password"id="c_password" name="c_password" value="<?php echo $c_password; ?>">
+    
 
     <br>
 
    <label style="font-family: Times New Roman,Times, serif;" >Blood Group:</label>
+        <select name="blood" id="blood">
+            <option>Select group</option>
 
-    <select name="blood" id="blood">Blood Group:
-        <option value="Select group">Select group</option>
-        <option value="A+">A+</option> 
-        <option value="B+">B+</option>
-        <option value="AB+">AB+</option> 
-        <option value="O+">O+</option>
-        <option value="A-">A-</option> 
-        <option value="B-">B-</option>
-        <option value="AB-">AB-</option> 
-        <option value="O-">O-</option>
-    
-    
-    </select>
+            <option value="A+" <?php if($blood=="A+") echo "selected"; ?>>A+</option>
+            
+            <option value="B+" <?php if($blood=="B+") echo "selected"; ?>>B+</option>
+            <option value="AB+" <?php if($blood=="AB+") echo "selected"; ?>>AB+</option>
+            <option value="O+" <?php if($blood=="O+") echo "selected"; ?>>O+</option>
+            <option value="A-" <?php if($blood=="A-") echo "selected"; ?>>A-</option>
+            <option value="B-" <?php if($blood=="B-") echo "selected"; ?>>B-</option>
+            <option value="AB-" <?php if($blood=="AB-") echo "selected"; ?>>AB-</option>
+            <option value="O-" <?php if($blood=="O-") echo "selected"; ?>>O-</option>
+        </select>
 
     <button type="submit" id="button">Register</button>
 
+    <div class="error">
+    <?php
+        echo $id_error;
+        echo "<br>";
+        echo $name_error;
+        echo "<br>";
+        echo $email_error;
+        echo "<br>";            
+        echo $phonenumber_error;
+        echo "<br>";
+        echo $password_error;
+        echo "<br>";
+        echo $blood_error;
+    ?>
+</div>
 
 
 
     </form>
-
-    <div id="error" name="error">
-    </div>
-
-    <?php
-
-    include '../PHP/validationonreg.php';
-
-    ?>
     
+
+    <?php 
+    
+    if (!empty($success_msg)) { ?>
+        <div class="success"><?php echo $success_msg; ?></div>
+    <?php } ?>
     
 </body>
 </html>
--- Final/Project-Hostel Mangement System/USER/PHP/validationonreg.php
+++ Final/Project-Hostel Mangement System/USER/PHP/validationonreg.php
@@ -1,51 +1,132 @@
 <?php
+
+$id = "";
+$name = "";
+$email = "";
+$phonenumber = "";
+$password = "";
+$c_password = "";
+$blood = "";
+
+$id_error = "";
+$name_error = "";
+$email_error = "";
+$phonenumber_error = "";
+$password_error = "";
+$c_password_error = "";
+$blood_error = "";
+
+$success_msg = "";
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $id= $_POST["id"];
-    $name= $_POST['name'];
-    $email= $_POST['email'];
-    $phonenumber= $_POST['phonenumber'];
-    $password= $_POST['password'];
-    $c_password= $_POST['c_password'];
-    $blood=$_POST["blood"];
-    $error="";
 
 
-   
-    if ($password !== $c_password) {
+    if(empty($_POST["id"])){
+        $id_error="Student ID is required";
+        
+       
+    }
+    else{
+        $id= trim($_POST["id"]);
 
-        $error="Passwords are not  correct!";
-        echo $error;
+    }
+
+    if(empty($_POST["name"])){
+        $name_error="Name is required";
+       
+       
+    }
+    else{
+        $name= trim($_POST["name"]);
+        if (!preg_match("/^[a-zA-Z ]*$/", $name)) {
+            $name_error = "Only letters and spaces allowed in here!!";
+        }
+
+    }
+    if(empty($_POST["email"])){
+        $email_error="Email is required";
+        
+       
+    }
+    else{
+        $email= trim($_POST["email"]);
+
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $email_error = "Invalid email format";
+        }
+
+    }
+    if(empty($_POST["phonenumber"])){
+        $phonenumber_error="Phone number is required";
+        
+       
+    }
+    else{
+        $phonenumber= trim($_POST["phonenumber"]);
+
+    }
+    if(empty($_POST["password"])){
+        $password_error="Password is required";
+        
+       
+    }
+    else{
+        if(strlen($_POST["password"]) < 6){
+            $password_error="Password must be at least 6 characters long";
+           
+       
+        }
+        $password= trim($_POST["password"]);
+
+    }
+    if(empty($_POST["c_password"])){
+        $c_password_error="Confirm your password";
+     
+       
+    }
+    else{
+        $c_password= trim($_POST["c_password"]);
+
+    }
+    if($password !== $c_password){
+        $password_error="Passwords do not match";
+        $c_password_error="Passwords do not match";
+       
+    }
+    if(empty($_POST["blood"]) || $_POST["blood"] == "Select group"){
+        $blood_error="Please select your blood group";
+        
+           }
+    else{
+        $blood= trim($_POST["blood"]);
+
+    }
+
+    if(empty($id_error) && empty($name_error) && empty($email_error) && empty($phonenumber_error) && empty($password_error) && empty($blood_error)){
+        $success_msg="Registration successful!";
+        echo $success_msg;
+        echo "<br>";
+        echo "Your Student ID is: " . $id."<br>";
+        echo "Name: " . $name."<br>";
+        echo "Email: " . $email."<br>";
+        echo "Phone Number: " . $phonenumber."<br>";
+        echo "Blood Group: " . $blood."<br>";
+        echo "<br>";
+
+
+        $id = "";
+        $name = "";
+        $email = "";
+        $phonenumber = "";
+        $password = "";
+        $c_password = "";
+        $blood = "";   
+
         exit();
         
-    }
-   
-    if(!preg_match("/^[a-zA-Z ]*$/",$name)){
-        echo "Names are allowed only";
-        exit();
 
-    }
-    if(empty($_POST["blood"])){
-        $error="Select your blood group";
-        echo $error;
-        exit(); 
+
 
     }
 
-    else{
-
-
-    echo "<h2>Registration Successful</h2>";
-
-
-    echo "Student ID: " . $id . "<br>";
-    echo "Name: " .$name. "<br>";
-    echo "Email: " .$email. "<br>";
-    echo "Phone Number: " .$phonenumber. "<br>";
-    echo "Password: " .$password. "<br>";
-    echo "Blood Group:" .$blood. "<br>";
-
-    exit();
-
-    }
 }
 ?>
